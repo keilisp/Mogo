3f31f2fc6005ec420e456b77e58bb2f18d48f7b9
mediocre <druksasha@ukr.net>
2019-03-28 02:01:08 +0200

    header

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
-$(function() {
+// $(function () {
+// 	if ($(window).width() < 576) {
+// 		$(function () {
+// 			$('#menu').slicknav({
+// 				label: '',
+// 				duration: 700,
+// 				closeOnClick: true,
+// 				duplicate: true
+// 			});
+// 		});
+// 	} else {}
+// });
+$(function () {
+			$('.navigation__menu').slicknav({
+				label: '',
+				duration: 500,
+				closeOnClick: true,
+				duplicate: true
+			});
 
-	// Custom JS
-
+			$
 });
--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slicknav/dist/jquery.slicknav.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
